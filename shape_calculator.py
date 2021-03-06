54f4ca3bb02bcd907b4d4f2df70cc44e7fcdc5ac
Bryan Wong <bryanwongzhicheng@gmail.com>
2021-03-06 16:57:15 -0500

    finish project

---
+++ shape_calculator.py
@@ -0,0 +1,58 @@
+import math
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+    
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+
+        str = ''
+
+        for i in range(0, self.height):
+            str += '\n'.rjust(self.width + 1, '*')
+        
+        return str
+        
+    def get_amount_inside(self, shape):
+        return math.floor(self.width / shape.width) * math.floor(self.height / shape.height)
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+class Square(Rectangle):
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+
+    def __str__(self):
+        return f"Square(side={self.width})"
+    
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
